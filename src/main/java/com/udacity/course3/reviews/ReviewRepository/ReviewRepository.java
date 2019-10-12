a7da76f1d7ee9a2feab7d1a7f0a6826238450189
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-10-12 17:39:37 +0100

    Added query logic

--- src/main/java/com/udacity/course3/reviews/ReviewRepository/ReviewRepository.java
+++ src/main/java/com/udacity/course3/reviews/ReviewRepository/ReviewRepository.java
@@ -1,4 +1,12 @@
 package com.udacity.course3.reviews.ReviewRepository;
 
-public class ReviewRepository {
+import com.udacity.course3.reviews.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	@Query("SELECT r FROM Review r WHERE r.product.productId = :productId")
+	List<Review> findAllByProductId(Integer productId);
 }
