ba30c3a4e94907ff233d7d05491b449ccfddface
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-10-12 16:59:40 +0100

    Completed product.

--- src/main/java/com/udacity/course3/reviews/entity/Product.java
+++ src/main/java/com/udacity/course3/reviews/entity/Product.java
@@ -7,8 +7,27 @@ import javax.persistence.*;
 public class Product {
 
 	@Id
-
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "product_id")
 	private Integer productId;
+
+
+	@Column(name = "product_name")
+	private String productName;
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
 }
