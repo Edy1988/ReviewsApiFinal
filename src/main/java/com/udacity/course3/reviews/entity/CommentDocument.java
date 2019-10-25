afcd21d2176a78565ec2f68d6b35475bbc5035e6
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-10-25 22:48:52 +0100

    Added Comment mongo document

---
+++ src/main/java/com/udacity/course3/reviews/entity/CommentDocument.java
@@ -0,0 +1,25 @@
+package com.udacity.course3.reviews.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("comments")
+public class CommentDocument {
+	private Integer commentId;
+	private String commentContent;
+
+	public Integer getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Integer commentId) {
+		this.commentId = commentId;
+	}
+
+	public String getCommentContent() {
+		return commentContent;
+	}
+
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+}
